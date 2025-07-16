c14de585a1c99d38461cd81d9ec751504c7dce2e
陈勇军 <10258926+yong-jun-chen@user.noreply.gitee.com>
2025-07-16 15:11:47 +0800

    创建celery.py，配置异步

--- blog/celery.py
+++ blog/celery.py
@@ -4,8 +4,8 @@ import os
 from celery import Celery
 
 # 设置 Django 的环境变量
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interview.settings')
 
-app = Celery('blog')
+app = Celery('interview')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
