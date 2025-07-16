72073823be076262d6968c6ea9a70d4778496505
陈勇军 <10258926+yong-jun-chen@user.noreply.gitee.com>
2025-07-16 10:18:11 +0800

    模型设计

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField()
+    total_views = models.IntegerField(default=0)  # 总阅读数
+    uv = models.IntegerField(default=0)   # 用户人次
+
+    class Meta:
+        verbose_name = '文章阅读统计'
+        verbose_name_plural = verbose_name
+
+
+class UserReadRecord(models.Model):
+    ip = models.CharField(max_length=30)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    pv = models.IntegerField(default=0)    # 用户阅读数
+    last_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = '用户阅读记录'
+        verbose_name_plural = verbose_name
